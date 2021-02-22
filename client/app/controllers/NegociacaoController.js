86e4609d9831ff07bdafda5b5fb87c70d6714bb7
Simplício Baiano <simpliciobaiano42@gmail.com>
2021-02-22 00:15:54 -0300

    cap 5, done, criacao de novo modelo

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -5,27 +5,35 @@ class NegociacaoController {
         this._inputData = $('#data')
         this._inputQuantidade = $('#quantidade')
         this._inputValor = $('#valor')
-
+        this._negociacoes = new Negociacoes()
     }
 
     adiciona(event)	{
         event.preventDefault()
+        
 
-        let data = DateConverter.paraData(this._inputData.value)
+        this._negociacoes.adiciona(this._criaNegociacao())
+        this._negociacoes.paraArray().length = 0
 
-        let negociacao = new  Negociacao(
-            data,
+        console.log(this._negociacoes.paraArray())
+
+        this._limpaFormulario()
+    }
+    
+    _limpaFormulario() {
+        this._inputData.value = ''
+        this._inputQuantidade.value = 1
+        this._inputValor.value = 0
+        this._inputData.focus() 
+    }
+
+    _criaNegociacao() {
+        
+        return new Negociacao(
+            DateConverter.paraData(this._inputData.value),
             parseInt(this._inputQuantidade.value),
             parseFloat(this._inputValor.value),
         )
-
-        console.log(negociacao)
-
-        let	diaMesAno = DateConverter.paraTexto(negociacao.data)
-
-        console.log(diaMesAno)
-
-        console.log(DateConverter.paraData('2017-11-12'))
 
     }
 
